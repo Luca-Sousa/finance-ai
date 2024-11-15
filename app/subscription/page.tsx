7cbd3a36e32c631e734c5a11ddb78e20a2dcef13
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-11-15 14:50:53 -0300

    feat: add stripe webhook that handles subscriptions

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import Navbar from "../_components/navbar";
 import { redirect } from "next/navigation";
 import {
@@ -10,10 +10,14 @@ import {
 } from "../_components/ui/card";
 import { CheckIcon, XIcon } from "lucide-react";
 import AcquirePlanButton from "./_components/acquire-plan-button";
+import { Badge } from "../_components/ui/badge";
 
 const SubscriptionPage = async () => {
   const { userId } = await auth();
   if (!userId) redirect("/login");
+
+  const user = await clerkClient().users.getUser(userId);
+  const hasPremiumPlan = user.publicMetadata.subscriptionPlan === "premium";
 
   return (
     <>
@@ -39,7 +43,7 @@ const SubscriptionPage = async () => {
             <CardContent className="space-y-6 py-8">
               <div className="flex items-center gap-2">
                 <CheckIcon className="text-primary" />
-                <p>Apenas 10 trsações por mês 7/10</p>
+                <p>Apenas 10 transações por mês 7/10</p>
               </div>
 
               <div className="flex items-center gap-2">
@@ -50,7 +54,13 @@ const SubscriptionPage = async () => {
           </Card>
 
           <Card className="w-[450px]">
-            <CardHeader className="border-b border-solid py-8">
+            <CardHeader className="relative border-b border-solid py-8">
+              {hasPremiumPlan && (
+                <Badge className="absolute left-4 top-12 bg-primary/10 text-primary">
+                  Ativo
+                </Badge>
+              )}
+
               <CardTitle className="text-center text-2xl font-semibold">
                 Plano Premium
               </CardTitle>
